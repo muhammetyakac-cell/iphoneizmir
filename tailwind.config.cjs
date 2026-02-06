bc7627fe4900d14d64f2e9bf61888998ef1698df
muhammetyakac-cell <muhammetyakac@gmail.com>
2026-02-06 04:15:14 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,7 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
-diff --git a/tailwind.config.cjs b/tailwind.config.cjs
-new file mode 100644
-index 0000000000000000000000000000000000000000..173282d1a10ba66f22e46fc22f84f9fda330196e
---- /dev/null
-+++ b/tailwind.config.cjs
-@@ -0,0 +1,7 @@
-+module.exports = {
-+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-+  theme: {
-+    extend: {},
-+  },
-+  plugins: [require('@tailwindcss/typography')],
-+};
- 
-EOF
-)
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
